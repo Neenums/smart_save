f0ec7b363a035c91dd5ae6ebf9a647d0d46dee54
Neenums <neenums18@gmail.com>
2024-07-05 17:58:48 +0530

    first commit

---
+++ lib/model/meta.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+
+Meta metaFromJson(String str) => Meta.fromJson(json.decode(str));
+String metaToJson(Meta data) => json.encode(data.toJson());
+class Meta {
+  Meta({
+      this.createdAt, 
+      this.updatedAt, 
+      this.barcode, 
+      this.qrCode,});
+
+  Meta.fromJson(dynamic json) {
+    createdAt = json['createdAt'];
+    updatedAt = json['updatedAt'];
+    barcode = json['barcode'];
+    qrCode = json['qrCode'];
+  }
+  String? createdAt;
+  String? updatedAt;
+  String? barcode;
+  String? qrCode;
+Meta copyWith({  String? createdAt,
+  String? updatedAt,
+  String? barcode,
+  String? qrCode,
+}) => Meta(  createdAt: createdAt ?? this.createdAt,
+  updatedAt: updatedAt ?? this.updatedAt,
+  barcode: barcode ?? this.barcode,
+  qrCode: qrCode ?? this.qrCode,
+);
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['createdAt'] = createdAt;
+    map['updatedAt'] = updatedAt;
+    map['barcode'] = barcode;
+    map['qrCode'] = qrCode;
+    return map;
+  }
+
+}
